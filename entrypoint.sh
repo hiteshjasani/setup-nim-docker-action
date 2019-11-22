2216f1a380ab86fa61e2f2fce7a4dc21f5a51caa
Hitesh Jasani <hitesh.jasani@gmail.com>
2019-11-22 18:49:42 -0500

    Export variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,13 @@
 echo "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
-nim --version
-nimble --version
-which nim
-which nimble
-nimble list -i --ver
+nim_ver=$(nim --version)
+echo ::set-output name=nim_version::$nim_ver
+nimble_ver=$(nimble --version)
+echo ::set-output name=nimble_version::$nimble_ver
+nim_bin_path=$(which nim)
+echo ::set-output name=nim_bin_path::$nim_bin_path
+nimble_bin_path=$(which nimble)
+echo ::set-output name=nimble_bin_path::$nimble_bin_path
+nimble_pkgs=$(nimble list -i --ver)
+echo ::set-output name=nimble_pkgs::$nimble_pkgs
